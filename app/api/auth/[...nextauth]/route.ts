c4d72ef18d60ddbc06fc6a557ed2b5a1a843e2f9
Thillai-G <thillai@gridapps.co>
2025-11-20 19:08:40 +0530

    installed latest next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -39,7 +39,7 @@ export const authOptions = {
   },
 
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user }: { token: any; user: any }) {
       if (user) {
         token.id = user.id;
         token.name = user.name;
@@ -48,7 +48,7 @@ export const authOptions = {
       return token;
     },
 
-    async session({ session, token }) {
+    async session({ session, token }: { session: any; token: any }) {
       session.user.id = token.id;
       session.user.name = token.name;
       session.user.email = token.email;
